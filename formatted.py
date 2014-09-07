f51e04c07f5bcf97f9147294913d243c48f84c7a
GongPA <david.chen@spsmail.cuny.edu>
2014-09-07 16:35:43 -0400

    6 digit number

--- formatted.py
+++ formatted.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:06.0f}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
