57b05bccb62f569aecdcd229d019be2e2048ece9
Valene Herrera <valene.herrera@spsmail.cuny.edu>
2014-09-20 19:24:48 -0400

    Split String Task 04

--- artists.py
+++ artists.py
@@ -5,3 +5,7 @@
 
 THE_GREAT_QUESTION = ('Michaelangelo. Leonardo. Rafael. Donatello. Turtles? '
                       'Creators of the great works? Both? You be the judge!')
+					  
+STATEMENTS = THE_GREAT_QUESTION.split()
+
+print STATEMENTS
