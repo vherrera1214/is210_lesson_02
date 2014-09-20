02fe0fa775660c06c3394eca3b9477bba8223152
Valene Herrera <valene.herrera@spsmail.cuny.edu>
2014-09-20 19:41:38 -0400

    Slicing a list Task 5

--- artists.py
+++ artists.py
@@ -6,5 +6,6 @@
 THE_GREAT_QUESTION = ('Michaelangelo. Leonardo. Rafael. Donatello. Turtles? '
                       'Creators of the great works? Both? You be the judge!')
 STATEMENTS = THE_GREAT_QUESTION.split()
+ARTISTS = STATEMENTS[0:4]
 
-print STATEMENTS
+print ARTISTS
