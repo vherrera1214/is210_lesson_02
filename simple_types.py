aa890c858f15e852cd621bedabfbb0043011b9e4
Valene Herrera <valene.herrera@spsmail.cuny.edu>
2014-09-21 20:46:24 -0400

    Task 17: Other Simple Data Types

---
+++ simple_types.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Contains other simple data types """
+
+IS_TRUE = True
+
+IS_FALSE = False
+
+IS_NONE = None
+
+INTEGER_EQUIVS = ((IS_TRUE == 1) and (IS_FALSE == 0)) 
